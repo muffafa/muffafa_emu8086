d6da1918a1ebd26b933ab991cfd343c2369d8a75
muffafa <mm.savar@gmail.com>
2022-06-05 23:26:34 +0300

    First Commit

---
+++ 1-1.asm
@@ -0,0 +1,11 @@
+;Write an Intel 8086 assembly code
+;that will write 34h value to 0100:1000h 
+;and 0100:2000h memory addresses.
+               
+MOV AX, 00100H
+MOV DS, AX
+
+MOV DS:[1000H], 034H
+MOV DS:[2000H], 034H
+
+RET
